10147adfeff63f703dee8d5788708e3505db0c27
Grace C <gchriste@uncc.edu>
2014-05-27 03:16:19 -0400

    Updating Github repo.

--- appointment.php
+++ appointment.php
@@ -1,20 +1,9 @@
 <?php
-// header('content-type: application/json; charset=utf-8');
-// header("access-control-allow-origin: *");
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+header('content-type: application/json; charset=utf-8');
+header("access-control-allow-origin: *");
 
-// Proccess the stuff from the form:
-$name = if $_POST["InputName"] ? $_POST["InputName"] : "not provided"; 
-$email = if $_POST["InputEmail"] ? $_POST["InputEmail"] : "not provided"; 
-
-?>
-
-<html>
-<body>
-
-Welcome <?= $name ?><br>
-Your email address is: <?php= $email ?>
-
-</body>
-</html>
-
+echo $_POST["InputName"];
+echo $_POST["InputEmail"];
 ?>
